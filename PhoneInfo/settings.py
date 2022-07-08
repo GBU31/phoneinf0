03fe1aef3086bc05ca811bcf7649e9deb977af02
brookehorizon <danieltashman17@gmail.com>
2022-07-08 16:50:11 +0300

    Updated

--- PhoneInfo/settings.py
+++ PhoneInfo/settings.py
@@ -123,9 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = [
-#   BASE_DIR / 'main/frontend/build/static'
-#  ]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'main/frontend/build/static')
 
